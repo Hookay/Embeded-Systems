4ad243d264fe50e4c34236d31cfa77a985b2d8e5
Hookay <72449804+Hookay@users.noreply.github.com>
2022-03-03 16:17:13 +0000

    Update test_time_Task1.ino
    Notes added task 1 10000.93 microseconds

--- Assignment2/test_time_Task1.ino
+++ Assignment2/test_time_Task1.ino
@@ -1,26 +1,36 @@
 
+//get pin
 #define B 26
-
+//add trigger signal variables
 float trig = 50;
 float rest =19950;
 
 
 void setup() {
+  //open serial port
   Serial.begin(9600);
+  //assign pin to output
   pinMode(B, OUTPUT);
+  //start counting
   unsigned long timeBegin = micros();
-  
+  //run it for 500 times
   for (int i = 0; i < 500; i++)
   {
+    //run code
+    //create 50 microseconds high
     digitalWrite(B, HIGH);
     delayMicroseconds(trig);
+    //create 19950 low state
     digitalWrite(B, LOW);
     delayMicroseconds(rest);
   }
-  
+  //stop counting
   unsigned long timeEnd = micros();
+  //getduration
   unsigned long duration = timeEnd - timeBegin;
+  //get average
   double averageDuration = (double)duration / 1000.0;
+  //show it in port
   Serial.println(averageDuration,DEC);
 }
 
