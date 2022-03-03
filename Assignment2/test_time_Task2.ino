c07a29d6e5c4ae45492d538a7201aaa6f9009a7a
Hookay <72449804+Hookay@users.noreply.github.com>
2022-03-03 16:10:31 +0000

    Create test_time_Task2.ino
    Time taken for task two is 0.0010milliseconds

---
+++ Assignment2/test_time_Task2.ino
@@ -0,0 +1,24 @@
+
+#define SW1 14
+
+
+void setup() {
+  //open serial port
+  Serial.begin(9600);
+  //assign pin to receive input
+  pinMode(SW1,INPUT);
+  //start measuring time in milisecond
+  unsigned long timeBegin = micros();
+     //run task
+     int s1 =digitalRead(SW1);
+  //end count
+  unsigned long timeEnd = micros();
+  //calculate elapsed time
+  unsigned long duration = timeEnd - timeBegin;
+  //get the average of the tasks
+  double averageDuration = (double)duration / 1000.0;
+  //print the result in the serial port
+  Serial.println(averageDuration,DEC);
+}
+
+void loop() {}
