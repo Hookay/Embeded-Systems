19850aa2493b48d716caa3566c52efd177b7b4b3
Hookay <72449804+Hookay@users.noreply.github.com>
2022-03-07 14:57:45 +0000

    Create test_time_Task4.ino
    0.083 microsecond

---
+++ Assignment2/test_time_Task4.ino
@@ -0,0 +1,34 @@
+//get pin
+
+#define AN 14
+
+
+void setup() {
+  //open serial port
+  Serial.begin(9600);
+  //assign pin to receive input
+  pinMode(AN,INPUT);
+
+  //start measuring time in milisecond
+  unsigned long timeBegin = micros();
+   for (int i = 0; i < 500; i++)
+  {
+     //run task
+     int s1 =digitalRead(AN);
+    // Serial.println(s1,DEC);
+     
+  }
+  //end count
+  unsigned long timeEnd = micros();
+  //calculate elapsed time
+  unsigned long duration = timeEnd - timeBegin;
+  //get the average of the tasks
+  double averageDuration = (double)duration / 1000.0;
+  //print the result in the serial port
+  Serial.println(averageDuration,DEC);
+}
+
+void loop() {
+
+
+}
