707bd055123a8c48274696b939fab2a5b7d117f9
Hookay <72449804+Hookay@users.noreply.github.com>
2022-03-07 15:55:32 +0000

    Create test_time_Task6.ino
    12.603 microseconds 10Hz

---
+++ Assignment2/test_time_Task6.ino
@@ -0,0 +1,38 @@
+//get pin
+
+//#define SIG 14
+
+
+void setup() {
+  //open serial port
+  Serial.begin(9600);
+  //assign pin to receive input
+ // pinMode(SIG,INPUT);
+
+  //start measuring time in milisecond
+  unsigned long timeBegin = micros();
+
+   for (int i = 0; i < 500; i++)
+  {
+      for (int i = 0; i < 1000; i++)
+    { 
+        __asm__ __volatile__ ("nop");
+      
+    }
+    // Serial.println(s1,DEC);
+     
+  }
+  //end count
+  unsigned long timeEnd = micros();
+  //calculate elapsed time
+  unsigned long duration = timeEnd - timeBegin;
+  //get the average of the tasks
+  double averageDuration = (double)duration / 1000.0;
+  //print the result in the serial port
+  Serial.println(averageDuration,DEC);
+}
+
+void loop() {
+
+
+}
