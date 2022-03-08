d7c1b5165bfb8714fefbb4ca3cf636c3668f5230
Hookay <72449804+Hookay@users.noreply.github.com>
2022-03-08 00:32:07 +0000

    Update test_time_Task5.ino
    0.002 microseconds

--- Assignment2/test_time_Task5.ino
+++ Assignment2/test_time_Task5.ino
@@ -1,25 +1,27 @@
 //get pin
 
-#define SIG 14
+#define PO 33
 
 
 void setup() {
   //open serial port
   Serial.begin(9600);
   //assign pin to receive input
-  pinMode(SIG,INPUT);
+  pinMode(PO,INPUT);
 
+  int pot=0;
+  for (int i = 0; i < 4; i++)
+  {
+     //run task
+    pot = pot+digitalRead(PO);
+
+  }
   //start measuring time in milisecond
   unsigned long timeBegin = micros();
    for (int i = 0; i < 500; i++)
-  {   int s1 =0;
-      for (int i = 0; i < 4; i++)
-    {
-        //run task
-        s1 = s1 + digitalRead(SIG);
-        // Serial.println(s1,DEC);
-    }
-    s1=s1/4;
+  {
+     int filter=pot/4;
+     //Serial.println(filter);
      
   }
   //end count
@@ -27,7 +29,7 @@ void setup() {
   //calculate elapsed time
   unsigned long duration = timeEnd - timeBegin;
   //get the average of the tasks
-  double averageDuration = (double)duration / 1000.0;
+  double averageDuration = (double)duration / 500.0;
   //print the result in the serial port
   Serial.println(averageDuration,DEC);
 }
