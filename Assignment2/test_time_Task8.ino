1bb567cc251053844bfc5653a508a1e46904769b
Hookay <72449804+Hookay@users.noreply.github.com>
2022-03-08 00:52:08 +0000

    Update test_time_Task8.ino
    0.18 microseconds

--- Assignment2/test_time_Task8.ino
+++ Assignment2/test_time_Task8.ino
@@ -1,52 +1,53 @@
 //get pin
-#define B 26
-#define SIG 14
+#define LED 32
+#define POT 14
 int error_code=0;
 
 void setup() {
   //open serial port
   Serial.begin(9600);
   //assign pin to receive input
-  pinMode(SIG,INPUT);
+  pinMode(POT,INPUT);
   //assign pin to output
-  pinMode(B, OUTPUT);
+  pinMode(LED, OUTPUT);
+
+
+  int s1 =0;
+  for (int i = 0; i < 4; i++)
+  {
+    //run task
+    s1 = s1 + digitalRead(POT);
+        
+  }
+  s1=s1/4;
+
+  if (s1 > 0.5){
+      error_code = 1;
+  }else{
+      error_code = 0;  
+  } 
 
   //start measuring time in milisecond
   unsigned long timeBegin = micros();
-       int s1 =0;
-      for (int i = 0; i < 4; i++)
-    {
-        //run task
-        s1 = s1 + digitalRead(SIG);
-        // Serial.println(s1,DEC);
-    }
-    s1=s1/4;
-
-    if (s1 > 0.5){
-      error_code = 1;
-    }else{
-      error_code = 0;
-    // Serial.println(s1,DEC);
-    } 
    for (int i = 0; i < 500; i++)
   {
 
     
-     //if switch is used
-     if(error_code==1){
-          //run code
-          //create 50 microseconds high to light up lamp with switch
-          digitalWrite(B, HIGH);
-      }else{
-           digitalWrite(B, LOW);
-      }
+    //if switch is used
+    if(error_code==1){
+       
+      //create 50 microseconds high to light up lamp with switch
+      digitalWrite(LED, HIGH);
+    }else{
+      digitalWrite(LED, LOW);
+    }
   }
   //end count
   unsigned long timeEnd = micros();
   //calculate elapsed time
   unsigned long duration = timeEnd - timeBegin;
   //get the average of the tasks
-  double averageDuration = (double)duration / 1000.0;
+  double averageDuration = (double)duration / 500.0;
   //print the result in the serial port
   Serial.println(averageDuration,DEC);
 }
