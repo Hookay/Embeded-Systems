ea5609f970d58df6df1a74260eb72aa3d5c37922
Hookay <72449804+Hookay@users.noreply.github.com>
2022-03-08 00:43:58 +0000

    Update test_time_Task7.ino
    0.003microseconds

--- Assignment2/test_time_Task7.ino
+++ Assignment2/test_time_Task7.ino
@@ -1,33 +1,33 @@
 //get pin
 
-#define SIG 14
+#define POT 14
 int error_code=0;
 
 void setup() {
   //open serial port
   Serial.begin(9600);
   //assign pin to receive input
-  pinMode(SIG,INPUT);
+  pinMode(POT,INPUT);
 
-  //start measuring time in milisecond
-  unsigned long timeBegin = micros();
-       int s1 =0;
-      for (int i = 0; i < 4; i++)
-    {
-        //run task
-        s1 = s1 + digitalRead(SIG);
-        // Serial.println(s1,DEC);
-    }
-    s1=s1/4;
-   for (int i = 0; i < 500; i++)
+  int s1 =0;
+
+  for (int i = 0; i < 4; i++)
   {
-       
+    //run task
+    s1 = s1 + digitalRead(POT);
+    // Serial.println(s1,DEC);
+  }
+  s1=s1/4;
 
+    //start measuring time in milisecond
+   unsigned long timeBegin = micros();
+   for (int i = 0; i < 500; i++)
+  {     
     if (s1 > 0.5){
       error_code = 1;
     }else{
       error_code = 0;
-    // Serial.println(s1,DEC);
+      
     } 
   }
   //end count
